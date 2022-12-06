ed3defb4c23d1e36127a8c937682c715685a3969
Mohammad Mohammad <mmohammadq8@outlook.com>
2022-12-06 21:05:35 +0300

    increase window and table height

--- src/main/java/io/momonet/portcheckerv2/HelloApplication.java
+++ src/main/java/io/momonet/portcheckerv2/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 350);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Port Reachability Checker");
         stage.setScene(scene);
         stage.show();
